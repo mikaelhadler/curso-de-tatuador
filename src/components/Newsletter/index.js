0a072a9d59df9e664c2d9e9abd82ddae29287a89
Mikael Hadler <mikaelhadler@gmail.com>
2020-06-30 20:44:14 -0300

    Update index.js

--- src/components/Newsletter/index.js
+++ src/components/Newsletter/index.js
@@ -52,25 +52,26 @@ export const Newsletter = ({ name: nameProp }) => {
 
   return (
     <div>
-      <form onSubmit={handleSubmit} action="/thank-you/">
-          <p>
-            <label>
-              Your Name: <input type="text" name="name" value={name} onChange={handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your Email: <input type="email" name="email" value={email} onChange={handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message: <textarea name="message" value={message} onChange={handleChange} />
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
+      <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">  <input type="hidden" name="bot-field" />  <input type="hidden" name="form-name" value="contact" />  <div className="field half first">
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" id="name" />
+          </div>
+          <div className="field half">
+            <label htmlFor="email">Email</label>
+            <input type="text" name="email" id="email" />
+          </div>
+          <div className="field">
+            <label htmlFor="message">Message</label>
+            <textarea name="message" id="message" rows="6" />
+          </div>
+          <ul className="actions">
+            <li>
+              <input type="submit" value="Send Message" className="special" />
+            </li>
+            <li>
+              <input type="reset" value="Clear" />
+            </li>
+          </ul>
         </form>
       </div>
   )
